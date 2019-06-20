230f1eb83c39f88d46772061ac7c70ea9b413ad4
Sen <YS@appledeMacBook-Air.local>
2019-06-19 22:33:56 -0400

    add drop down

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import dash_table
 
 from sklearn.externals import joblib
 import plotly.graph_objs as go
-
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -24,39 +23,77 @@ app.title = 'MeCan'
 model_dir = 'models/'
 model_paths = [join(model_dir, f) for f in listdir(model_dir) if isfile(join(model_dir, f))]
 models = [joblib.load(i) for i in model_paths]
-#model = joblib.load("./finalized_model.sav")
+opts = [{'label' : "patient {}".format(i), 'value' : "patient{}".format(i)} for i in range(1,4)]
+
 
 app.layout = html.Div(children=[
-    html.H1(children='Welcome to MeCan!'),
+                                html.H1(children='Welcome to MeCan!', style={'textAlign': 'center', 'backgroundColor':'#98C0B9'}),
                                  
     html.Div(children='''
-    Upload your gene expression profile and find out your personalized sensitivity to the common chemotheraputic drugs.
-    ''', style={'fontSize': 24}),
-                                 
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-                         'Drag and Drop or ',
-                         html.A('Select Files')
-                         ]),
-        style={
-        'width': '100%',
-        'height': '60px',
-        'lineHeight': '60px',
-        'borderWidth': '1px',
-        'borderStyle': 'dashed',
-        'borderRadius': '5px',
-        'textAlign': 'center',
-        'margin': '10px'
-        },
-        # Do not allow multiple files to be uploaded
-        multiple=True
-        ),
+    Find out your personalized sensitivity to the common chemotheraputic drugs.
+    ''', style={'fontSize': 24, 'marginBottom': '1.5em'}),
+                                
+    html.Div(children='''Select to see an examples, or upload your gene expression file:''',
+             style={'fontSize': 18}),
+
+    html.Div([
+              html.Div([
+                  dcc.Dropdown(
+                               id='example',
+                               options=opts,
+                               style={
+                               'fontsize': '12px',
+                               'height': '60px',
+                               'lineHeight': '60px',
+                               'borderWidth': '1px',
+                               'textAlign': 'center',
+                               'margin': '10px'
+                               }
+                               )
+                        ],style={'width': '48%', 'display': 'inline-block'}),
+              html.Div([
+                  dcc.Upload(
+                             id='upload-data',
+                             children=html.Div([
+                                                'Drag and Drop or ',
+                                                html.A('Select Files')
+                                                ]),
+                             style={
+                             'height': '60px',
+                             'lineHeight': '60px',
+                             'borderWidth': '1px',
+                             'borderStyle': 'dashed',
+                             'borderRadius': '5px',
+                             'textAlign': 'center',
+                             'margin': '10px'
+                             },
+                             # Do not allow multiple files to be uploaded
+                             multiple=True
+                             )
+                        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+              ]),
+             
     html.Div(id='output-data-upload'),
     ])
 
 
-def parse_contents(contents, filename, date):
+@app.callback(Output('output-data-upload', 'children'),
+              [Input('example', 'value'),
+               Input('upload-data', 'contents')],
+              [State('upload-data', 'filename')])
+def update_output(patient_num, list_of_contents, list_of_names):
+    if patient_num is not None:
+        filepath = 'examples/' + str(patient_num) + '.csv'
+        df = pd.read_csv(filepath, header=None)
+
+        return run_models(patient_num + '.csv', df)
+    if list_of_contents is not None:
+        children = [parse_contents(c, n) for c, n in
+                    zip(list_of_contents, list_of_names)]
+        return children
+
+
+def parse_contents(contents, filename):
     content_type, content_string = contents.split(',')
     
     decoded = base64.b64decode(content_string)
@@ -70,51 +107,33 @@ def parse_contents(contents, filename, date):
     except Exception as e:
         print(e)
         return html.Div([
-                     'There was an error processing this file.'
-                     ])
+                         'There was an error processing this file.'
+                         ])
+    return run_models(filename, df)
+
+def run_models(filename, df):
     ic50s = []
     for model in models:
         ic50s.append(model.predict(df.T)[1])
+    drug_names = [i[7:-10] for i in model_paths]
+    print(drug_names)
+    df1 = pd.DataFrame({'Drug Name': drug_names,
+                      'IC50': ic50s})
 
     return html.Div([
-                    html.H5("File name: " + filename),
-                    html.H6("Last modified time: " + str(datetime.datetime.fromtimestamp(date))),
+                     html.H5('Input from: ' + filename),
                      
-                    html.Div(', '.join([str(i) for i in ic50s])),
+                     dash_table.DataTable(
+                                          data=df1.to_dict('records'),
+                                          columns=[{"name": i, "id": i} for i in df1.columns],
+                                          style_cell = {
+                                          'font_size': '16px',
+                                          'text_align': 'center'
+                                          }
+                                          ),
                      
-                    dash_table.DataTable(
-                                       data=df.to_dict('records'),
-                                       columns=[{'name': i, 'id': i} for i in df.columns]
-                                       ),
-
-                    html.Hr(),  # horizontal line
-
-                    # For debugging, display the raw contents provided by the web browser
-                    #html.Div('Raw Content'),
-                    #html.Pre(contents[0:200] + '...', style={
-                    #       'whiteSpace': 'pre-wrap',
-                    #      'wordBreak': 'break-all'
-                    #      })
-                    ])
-
-@app.callback(Output('output-data-upload', 'children'),
-              [Input('upload-data', 'contents')],
-              [State('upload-data', 'filename'),
-               State('upload-data', 'last_modified')])
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [parse_contents(c, n, d) for c, n, d in
-                    zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
-
-
-def update_years_of_experience_input(years_of_experience):
-    if years_of_experience is not None and years_of_experience is not '':
-        try:
-            salary = model.predict(float(years_of_experience))[0]
-            return 'With {} years of experience you should earn a salary of ${:,.2f}'.                format(years_of_experience, salary, 2)
-        except ValueError:
-            return 'Unable to give years of experience'
+                     html.Hr(),  # horizontal line
+                     ])
 
 
 if __name__ == '__main__':
